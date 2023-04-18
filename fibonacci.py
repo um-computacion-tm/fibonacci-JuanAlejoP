82f37935e2a0e31437384463208adef0547c4cd8
JuanAlejoP <j.patino@alumno.um.edu.ar>
2023-04-18 08:10:27 -0300

    Actualización menor.

--- fibonacci.py
+++ fibonacci.py
@@ -1,2 +1,18 @@
-def fibonacci():
-    return
+def fibonacci(number):
+    fibonacci_numbers = [0, 1]
+    total = 1
+    print("number " + str(number))
+    for index in range(2, number+1):
+        total = (
+            fibonacci_numbers[-2] +
+            fibonacci_numbers[-1]
+        )
+        fibonacci_numbers.append(total)
+        print("fibonacci_numbers " + str(fibonacci_numbers))
+        print("total " + str(total))
+    return total
+
+def fibonacci_1(number):
+    if number <= 1:
+        return number
+    return fibonacci(number - 2) + fibonacci(number - 1)
--- test.py
+++ test.py
@@ -1,13 +1,26 @@
 import unittest
-from fibonacci import fibonacci
-
-
-
+from fibonacci import fibonacci, fibonacci_1
 
 
 class TestFibonacci(unittest.TestCase):
-    def test01(self):
+    def test_1(self):
         self.assertEqual(1, fibonacci(1))
+
+    def test_2(self):
+        self.assertEqual(1, fibonacci(2))
+
+    def test_3(self):
+        self.assertEqual(2, fibonacci(3))
+
+    def test_4(self):
+        self.assertEqual(3, fibonacci(4))
+
+    def test_5(self):
+        self.assertEqual(5, fibonacci(5))
+
+    def test_6(self):
+        self.assertEqual(8, fibonacci(6))
+
 
 if __name__ == '__main__':
     unittest.main()
